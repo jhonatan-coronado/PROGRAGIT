99081eb46ebdd24f5e88b50faf9db2a725583024
Jhonatan-Coronado <jhonatan2000coro@gmail.com>
2022-09-23 08:35:21 -0400

    clase 1

--- BIGIT/src/app/app.component.spec.ts
+++ BIGIT/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'brogit'`, () => {
+  it(`should have as title 'BIGIT'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('brogit');
+    expect(app.title).toEqual('BIGIT');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('brogit app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('BIGIT app is running!');
   });
 });
