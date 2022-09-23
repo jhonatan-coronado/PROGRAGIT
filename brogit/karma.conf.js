01a85553bdd430a7e8e77e1108f2106e4bae130a
Jhonatan-Coronado <jhonatan2000coro@gmail.com>
2022-09-23 08:16:25 -0400

    version o prueba 1

--- brogit/karma.conf.js
+++ brogit/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/exagit'),
+      dir: require('path').join(__dirname, './coverage/brogit'),
       subdir: '.',
       reporters: [
         { type: 'html' },
--- brogit/src/app/app.component.spec.ts
+++ brogit/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'EXAGIT'`, () => {
+  it(`should have as title 'brogit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('EXAGIT');
+    expect(app.title).toEqual('brogit');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('EXAGIT app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('brogit app is running!');
   });
 });
